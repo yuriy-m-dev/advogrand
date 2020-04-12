250f7b73018d89eb407a268849666d4864f03f3b
yuriy-m-dev <fath66426@gmail.com>
2020-04-12 12:46:34 +0300

    fix bag

--- app/js/main.js
+++ app/js/main.js
@@ -58,4 +58,50 @@ $(function(){
       });
       return false;
     });
+
+    // const mobileBtn = document.querySelectorAll('.header-top__inner-menu-link');
+
+    $('.header-top__inner-menu-link').click(function() {
+      $('.header-top__inner-menu').removeClass('active');
+    });
+
+    // Scroll to anchors
+    (function () {
+
+      const smoothScroll = function (targetEl, duration) {
+          const headerElHeight =  document.querySelector('.header-top').clientHeight;
+          let target = document.querySelector(targetEl);
+          let targetPosition = target.getBoundingClientRect().top - headerElHeight;
+          let startPosition = window.pageYOffset;
+          let startTime = null;
+      
+          const ease = function(t,b,c,d) {
+              t /= d / 2;
+              if (t < 1) return c / 2 * t * t + b;
+              t--;
+              return -c / 2 * (t * (t - 2) - 1) + b;
+          };
+      
+          const animation = function(currentTime){
+              if (startTime === null) startTime = currentTime;
+              const timeElapsed = currentTime - startTime;
+              const run = ease(timeElapsed, startPosition, targetPosition, duration);
+              window.scrollTo(0,run);
+              if (timeElapsed < duration) requestAnimationFrame(animation);
+          };
+          requestAnimationFrame(animation);
+
+      };
+
+      const scrollTo = function () {
+          const links = document.querySelectorAll('.js-scroll');
+          links.forEach(each => {
+              each.addEventListener('click', function () {
+                  const currentTarget = this.getAttribute('href');
+                  smoothScroll(currentTarget, 1000);
+              });
+          });
+      };
+      scrollTo();
+  }());
 });
